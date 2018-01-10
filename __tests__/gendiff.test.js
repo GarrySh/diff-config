d7d030f8626f26571b536343183ccf966c714386
GarrySh <sooo.lame@gmail.com>
2018-01-10 17:36:42 +1000

    add JSON recursive test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,15 +10,49 @@ describe('simple data', () => {
   + verbose: true
 }`;
 
-  test('diff JSON flat files ', () => {
-    expect(gendiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(expected);
+  test('diff JSON files ', () => {
+    expect(gendiff('__tests__/__fixtures__/before_flat.json', '__tests__/__fixtures__/after_flat.json')).toBe(expected);
   });
 
-  test('diff YAML flat files ', () => {
-    expect(gendiff('__tests__/__fixtures__/before.yaml', '__tests__/__fixtures__/after.yaml')).toBe(expected);
+  test('diff YAML files ', () => {
+    expect(gendiff('__tests__/__fixtures__/before_flat.yaml', '__tests__/__fixtures__/after_flat.yaml')).toBe(expected);
   });
 
-  test('diff INI flat files ', () => {
-    expect(gendiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini')).toBe(expected);
+  test('diff INI files ', () => {
+    expect(gendiff('__tests__/__fixtures__/before_flat.ini', '__tests__/__fixtures__/after_flat.ini')).toBe(expected);
+  });
+});
+
+describe('recursive data', () => {
+  const expected = `
+{
+  common: {
+      setting1: Value 1
+    - setting2: 200
+      setting3: true
+      setting6: {
+          key: value
+        + ops: vops
+      }
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+  }
+  group1: {
+    + baz: bars
+    - baz: bas
+      foo: bar
+  }
+- group2: {
+      abc: 12345
+  }
++ group3: {
+      fee: 100500
+  }
+}`;
+
+  test('diff JSON files ', () => {
+    expect(gendiff('__tests__/__fixtures__/before_flat.json', '__tests__/__fixtures__/after_flat.json')).toBe(expected);
   });
 });
