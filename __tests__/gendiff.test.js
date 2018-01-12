bd5fa6b7cb3c9bbc9ad3fa1dfddb2ad226deea4b
GarrySh <sooo.lame@gmail.com>
2018-01-12 13:50:02 +1000

    add compare reqursive files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,7 @@
 import gendiff from '../src';
 
-describe('simple data', () => {
-  const expected = `
-{
+describe('flat data', () => {
+  const expected = `{
     host: hexlet.io
   + timeout: 20
   - timeout: 50
@@ -24,35 +23,42 @@ describe('simple data', () => {
 });
 
 describe('recursive data', () => {
-  const expected = `
-{
-  common: {
-      setting1: Value 1
-    - setting2: 200
-      setting3: true
-      setting6: {
-          key: value
-        + ops: vops
-      }
-    + setting4: blah blah
-    + setting5: {
-          key5: value5
-      }
-  }
-  group1: {
-    + baz: bars
-    - baz: bas
-      foo: bar
-  }
-- group2: {
-      abc: 12345
-  }
-+ group3: {
-      fee: 100500
-  }
+  const expected = `{
+    common: {
+        setting1: Value 1
+      - setting2: 200
+        setting3: true
+        setting6: {
+            key: value
+          + ops: vops
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
 }`;
 
   test('diff JSON files ', () => {
-    expect(gendiff('__tests__/__fixtures__/before_flat.json', '__tests__/__fixtures__/after_flat.json')).toBe(expected);
+    expect(gendiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(expected);
+  });
+
+  test('diff YML files ', () => {
+    expect(gendiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml')).toBe(expected);
+  });
+
+  test('diff INI files ', () => {
+    expect(gendiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini')).toBe(expected);
   });
 });
--- src/index.js
+++ src/index.js
@@ -25,64 +25,103 @@ const parseFile = (pathToFile) => {
 
 const propertyCompare = [
   {
-    type: 'unchanged',
+    type: 'added_node',
+    check: (before, after) => _.isUndefined(before) && _.isObject(after),
+    getChildren: (func, before, after) => func(after, after),
+  },
+  {
+    type: 'deleted_node',
+    check: (before, after) => _.isObject(before) && _.isUndefined(after),
+    getChildren: (func, before) => func(before, before),
+  },
+  {
+    type: 'node',
+    check: (before, after) => _.isObject(before) && _.isObject(after),
+    getChildren: (func, before, after) => func(before, after),
+  },
+  {
+    type: 'unchanged_leaf',
     check: (before, after) => before === after,
+    getChildren: () => [],
   },
   {
-    type: 'added',
+    type: 'added_leaf',
     check: before => _.isUndefined(before),
+    getChildren: () => [],
   },
   {
-    type: 'deleted',
+    type: 'deleted_leaf',
     check: (before, after) => _.isUndefined(after),
+    getChildren: () => [],
   },
   {
-    type: 'changed',
+    type: 'changed_leaf',
     check: (before, after) => before !== after,
+    getChildren: () => [],
+  },
+  {
+    // Error of data type
+    check: () => { throw new Error('unsupported data type'); },
   },
 ];
 
 const getPropertyOfCompare = (valueBefore, valueAfter) => _.find(propertyCompare, ({ check }) =>
   check(valueBefore, valueAfter));
 
-const getAst = (fileBefore, fileAfter) => {
+const getAst = (fileBefore = {}, fileAfter = {}) => {
   const uniqKeys = _.union(_.keys(fileBefore), _.keys(fileAfter));
   return uniqKeys.map((key) => {
     const valueBefore = fileBefore[key];
     const valueAfter = fileAfter[key];
-    const { type } = getPropertyOfCompare(valueBefore, valueAfter);
-    return { key, valueBefore, valueAfter, type };
+    const { type, getChildren } = getPropertyOfCompare(valueBefore, valueAfter);
+    const children = getChildren(getAst, valueBefore, valueAfter);
+    return { key, valueBefore, valueAfter, type, children };
   });
 };
 
 const propertyRender = [
   {
-    type: 'unchanged',
-    action: (key, before) => `    ${key}: ${before}`,
+    type: 'added_node',
+    action: (nestingLevel, key, before, after, renderChildren) => `${' '.repeat((nestingLevel * 4) + 2)}+ ${key}: ${renderChildren}`,
   },
   {
-    type: 'added',
-    action: (key, before, after) => `  + ${key}: ${after}`,
+    type: 'deleted_node',
+    action: (nestingLevel, key, before, after, renderChildren) => `${' '.repeat((nestingLevel * 4) + 2)}- ${key}: ${renderChildren}`,
   },
   {
-    type: 'deleted',
-    action: (key, before) => `  - ${key}: ${before}`,
+    type: 'node',
+    action: (nestingLevel, key, before, after, renderChildren) => `${' '.repeat((nestingLevel * 4) + 2)}  ${key}: ${renderChildren}`,
   },
   {
-    type: 'changed',
-    action: (key, before, after) => `  + ${key}: ${after}\n  - ${key}: ${before}`,
+    type: 'unchanged_leaf',
+    action: (nestingLevel, key, before) => `${' '.repeat((nestingLevel * 4) + 2)}  ${key}: ${before}`,
+  },
+  {
+    type: 'added_leaf',
+    action: (nestingLevel, key, before, after) => `${' '.repeat((nestingLevel * 4) + 2)}+ ${key}: ${after}`,
+  },
+  {
+    type: 'deleted_leaf',
+    action: (nestingLevel, key, before) => `${' '.repeat((nestingLevel * 4) + 2)}- ${key}: ${before}`,
+  },
+  {
+    type: 'changed_leaf',
+    action: (nestingLevel, key, before, after) => `${' '.repeat((nestingLevel * 4) + 2)}+ ${key}: ${after}\n${' '.repeat((nestingLevel * 4) + 2)}- ${key}: ${before}`,
   },
 ];
 
 const getPropertyOfRender = type => _.find(propertyRender, { type });
 
-const renderDiff = (ast) => {
+// const renderDiff = ast => ast;
+
+const renderDiff = (ast, nestingLevel = 0) => {
   const diff = ast.map((value) => {
-    const { key, valueBefore, valueAfter, type } = value;
+    const { key, valueBefore, valueAfter, type, children } = value;
+    const renderChildren = renderDiff(children, nestingLevel + 1);
     const { action } = getPropertyOfRender(type);
-    return action(key, valueBefore, valueAfter);
+    return action(nestingLevel, key, valueBefore, valueAfter, renderChildren);
   }).join('\n');
-  const result = ['', '{', diff, '}'];
+  const result = ['{', diff, `${' '.repeat(nestingLevel * 4)}}`];
   return result.join('\n');
 };
 
